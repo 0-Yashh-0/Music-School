b3e2908d5f87f4ed688df2f3c5d4409d3facc95a
0-Yashh-0 <yash-ug22@nsut.ac.in>
2024-12-21 00:29:35 +0530

    Footer added

--- musicapp/src/app/page.tsx
+++ musicapp/src/app/page.tsx
@@ -4,6 +4,7 @@ import StickyScrollReveal from "@/components/WhyChooseUs";
 import Reviews from "@/components/Reviews";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="overflow-auto scrollbar-hide min-h-screen bg-black/[0.97] antialiased bg-grid-white/[0.02]">
@@ -13,6 +14,7 @@ export default function Home() {
       <Reviews />
       <UpcomingWebinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }
